208eecae8bbb11a1f96c48cfdc8d3ae0969084c4
Simran <w0823737@myscc.ca>
2024-03-17 23:58:56 -0400

    i created my project as given instructions

--- SimranBookStore/Startup.cs
+++ SimranBookStore/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using SimranBookStore.Data;
+using SimranBookStore.DataAccess.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -63,7 +63,7 @@ namespace SimranBookStore
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
--- SimranBooks.DataAccess/Data/ApplicationDbContext.cs
+++ SimranBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace SimranBookStore.Data
+namespace SimranBookStore.DataAccess.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {

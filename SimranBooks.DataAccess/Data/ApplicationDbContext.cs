9d1f91404265882225dc3b5f1b2ff95f68991fef
Simran <w0823737@myscc.ca>
2024-03-24 17:16:21 -0400

    create new files as required and modify

--- SimranBooks.DataAccess/Data/ApplicationDbContext.cs
+++ SimranBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using SimranBooks.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,8 @@ namespace SimranBookStore.DataAccess.Data
             : base(options)
         {
         }
+
+            public DbSet<Category> Categories { get; set; }
+        
     }
 }

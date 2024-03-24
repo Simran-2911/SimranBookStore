692ea6879b503ff43865f370dea6df15d330ac77
Simran <w0823737@myscc.ca>
2024-03-24 17:46:03 -0400

    added more files

---
+++ SimranBooks.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using SimranBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimranBooks.DataAccess.Repository.IRepository
+{
+   public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+    }
+}
